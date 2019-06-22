a44c6b2883e8d35ddb8483248b4f92bb6a8ebb4a
irenedeni <irene@codaisseur.com>
2019-06-22 14:52:08 +0200

    country selection returning correct data from API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,70 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
+import { graphql } from "gatsby"
+import axios from 'axios'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export const GatsbyQuery = graphql`
+{
+  countriesApi {
+    countries {
+      name
+      code
+      currency
+    }
+  }
+}
+`
 
-export default IndexPage
+class Country extends Component {
+  state = {
+    country: {
+      alpha2Code: '',
+      name: '',
+      population: 0,
+    }
+  }
+  
+  render() {
+    const {
+     countriesApi: { countries },
+    } = this.props.data;
+
+    
+    console.log("this.props.data", countries)
+    console.log("state.code", this.state.country.code)
+    console.log("state.country", this.state.country.name)
+    console.log("state.population", this.state.country.population)
+
+
+    return (
+      <div style={{ textAlign: "center", width: "600px", margin: "50px auto" }}>
+        <p>Choose the country below</p>
+        <select value={this.state.country.alpha2Code} onChange={this.onCountryChange}>
+          {countries.map(country => (
+            <option key={country.code} value={country.code}>
+              {country.code}
+            </option>
+          ))}
+         </select> 
+         
+        <p>COUNTRY name: {this.state.country.name}</p> 
+        <p>COUNTRY population: {this.state.country.population}</p> 
+     </div>
+    )
+  }
+  onCountryChange = event => {
+    event.preventDefault();
+    console.log("event.target.value", event.target.value)
+    const code = event.target.value
+    console.log("code", code)
+    axios
+    .get(`https://restcountries.eu/rest/v2/alpha/${code}`)
+    .then(response => { 
+      console.log("response", response.data.alpha2Code);
+      this.setState({ country: response.data })
+    })
+  };
+}
+
+
+export default Country
